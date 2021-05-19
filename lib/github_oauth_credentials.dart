c9c580a086c6a4279862ab749236be0dff3cb643
mmk01569 <mmk01529@gmail.com>
2021-05-19 09:13:28 -0400

    initial commit

---
+++ lib/github_oauth_credentials.dart
@@ -0,0 +1,6 @@
+// TODO(CodelabUser): Create an OAuth App
+const githubClientId = '02d8151fa5faba543daf';
+const githubClientSecret = '1c82e97f7eeda8b58014b66330dd3bc2f664edf5';
+
+// OAuth scopes for repository and user information
+const githubScopes = ['repo', 'read:org'];
